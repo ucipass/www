d059af4460ae8a35ffe6a7fda5dc590f0ba66d0c
ucipass@gmail.com <G9resume1>
2020-04-13 10:52:32 -0500

    delete old files

--- frontend/old2/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    configureWebpack: {             // VS Code debugging
-        devtool: 'source-map'
-      },    
-    outputDir: "../backend/dist"    // Build directory
-  }
